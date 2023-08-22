cfd057fda49e7ba708a8fbcdb0534ddec7fd10df
박준범 <@DESKTOP-F3IEPRS>
2023-08-23 08:37:22 +0900

    aa

--- TeamProject/src/main/java/com/itwillbs/controller/HostMemberController.java
+++ TeamProject/src/main/java/com/itwillbs/controller/HostMemberController.java
@@ -4,15 +4,11 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.itwillbs.service.MemberService;
-
 public class HostMemberController extends HttpServlet {
-
-	RequestDispatcher dispatcher =null;
-	MemberService memberService = null;
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
