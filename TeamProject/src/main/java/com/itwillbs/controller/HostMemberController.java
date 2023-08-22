84ee3d6b4036b657fd725eae9875d0814b368ea9
박준범 <@DESKTOP-F3IEPRS>
2023-08-23 08:22:25 +0900

    alsifj

---
+++ TeamProject/src/main/java/com/itwillbs/controller/HostMemberController.java
@@ -0,0 +1,33 @@
+package com.itwillbs.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.itwillbs.service.MemberService;
+
+public class HostMemberController extends HttpServlet {
+
+	RequestDispatcher dispatcher =null;
+	MemberService memberService = null;
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("MemberController doGet()");
+		doProcess(request, response);
+	}//doGet()
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("MemberController doPost()");
+		doProcess(request, response);
+	}//doPost()
+	
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+	}
+}
