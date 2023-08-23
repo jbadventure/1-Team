aab9795744b3c4cf1e831f8027fde3e399465a71
박준범 <@DESKTOP-F3IEPRS>
2023-08-23 09:02:48 +0900

    계속 수정중

---
+++ TeamProject/src/main/java/com/itwillbs/domain/GuestMemberDTO.java
@@ -0,0 +1,84 @@
+package com.itwillbs.domain;
+
+public class GuestMemberDTO {
+	
+	private int guestNum;
+	private String guestId;
+	private String guestPassword;
+	private String guestName;
+	private String guestNickname;
+	private String guestGender;
+	private String guestBirthday;
+	private String guestPhoneNum;
+	private String guestEmail;
+	private String guestLocation;
+	private String guestProfileImagePath;
+	
+	public int getGuestNum() {
+		return guestNum;
+	}
+	public void setGuestNum(int guestNum) {
+		this.guestNum = guestNum;
+	}
+	public String getGuestId() {
+		return guestId;
+	}
+	public void setGuestId(String guestId) {
+		this.guestId = guestId;
+	}
+	public String getGuestPassword() {
+		return guestPassword;
+	}
+	public void setGuestPassword(String guestPassword) {
+		this.guestPassword = guestPassword;
+	}
+	public String getGuestName() {
+		return guestName;
+	}
+	public void setGuestName(String guestName) {
+		this.guestName = guestName;
+	}
+	public String getGuestNickname() {
+		return guestNickname;
+	}
+	public void setGuestNickname(String guestNickname) {
+		this.guestNickname = guestNickname;
+	}
+	public String getGuestGender() {
+		return guestGender;
+	}
+	public void setGuestGender(String guestGender) {
+		this.guestGender = guestGender;
+	}
+	public String getGuestBirthday() {
+		return guestBirthday;
+	}
+	public void setGuestBirthday(String guestBirthday) {
+		this.guestBirthday = guestBirthday;
+	}
+	public String getGuestPhoneNum() {
+		return guestPhoneNum;
+	}
+	public void setGuestPhoneNum(String guestPhoneNum) {
+		this.guestPhoneNum = guestPhoneNum;
+	}
+	public String getGuestEmail() {
+		return guestEmail;
+	}
+	public void setGuestEmail(String guestEmail) {
+		this.guestEmail = guestEmail;
+	}
+	public String getGuestLocation() {
+		return guestLocation;
+	}
+	public void setGuestLocation(String guestLocation) {
+		this.guestLocation = guestLocation;
+	}
+	public String getGuestProfileImagePath() {
+		return guestProfileImagePath;
+	}
+	public void setGuestProfileImagePath(String guestProfileImagePath) {
+		this.guestProfileImagePath = guestProfileImagePath;
+	}
+	
+}
---
+++ TeamProject/src/main/java/com/itwillbs/domain/HostMemberDTO.java
@@ -0,0 +1,92 @@
+package com.itwillbs.domain;
+
+public class HostMemberDTO {
+
+	private int hostNum;
+	private String hostId;
+	private String hostPassword;
+	private String hostName;
+	private String hostNickname;
+	private String hostGender;
+	private String hostBirthday;
+	private String hostPhoneNum;
+	private String hostEmail;
+	private String businessNum;
+	private String mailOrderNum;
+	private String hostProfileImagePath;
+	
+	public int getHostNum() {
+		return hostNum;
+	}
+	public void setHostNum(int hostNum) {
+		this.hostNum = hostNum;
+	}
+	public String getHostId() {
+		return hostId;
+	}
+	public void setHostId(String hostId) {
+		this.hostId = hostId;
+	}
+	public String getHostPassword() {
+		return hostPassword;
+	}
+	public void setHostPassword(String hostPassword) {
+		this.hostPassword = hostPassword;
+	}
+	public String getHostName() {
+		return hostName;
+	}
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
+	public String getHostNickname() {
+		return hostNickname;
+	}
+	public void setHostNickname(String hostNickname) {
+		this.hostNickname = hostNickname;
+	}
+	public String getHostGender() {
+		return hostGender;
+	}
+	public void setHostGender(String hostGender) {
+		this.hostGender = hostGender;
+	}
+	public String getHostBirthday() {
+		return hostBirthday;
+	}
+	public void setHostBirthday(String hostBirthday) {
+		this.hostBirthday = hostBirthday;
+	}
+	public String getHostPhoneNum() {
+		return hostPhoneNum;
+	}
+	public void setHostPhoneNum(String hostPhoneNum) {
+		this.hostPhoneNum = hostPhoneNum;
+	}
+	public String getHostEmail() {
+		return hostEmail;
+	}
+	public void setHostEmail(String hostEmail) {
+		this.hostEmail = hostEmail;
+	}
+	public String getBusinessNum() {
+		return businessNum;
+	}
+	public void setBusinessNum(String businessNum) {
+		this.businessNum = businessNum;
+	}
+	public String getMailOrderNum() {
+		return mailOrderNum;
+	}
+	public void setMailOrderNum(String mailOrderNum) {
+		this.mailOrderNum = mailOrderNum;
+	}
+	public String getHostProfileImagePath() {
+		return hostProfileImagePath;
+	}
+	public void setHostProfileImagePath(String hostProfileImagePath) {
+		this.hostProfileImagePath = hostProfileImagePath;
+	}
+	
+	
+}
