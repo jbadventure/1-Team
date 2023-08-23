9ba48f968e2b2d6b2827f6273af5f8b01e390c19
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2023-08-23 12:46:34 +0900

    RequestDispatcher dispatcher =null; GuestMemberService guestMemberService = null;
    멤버변수 추가
    
    member폴더 생성하고 login.jsp 복붙

--- TeamProject/src/main/java/com/itwillbs/controller/GuestMemberController.java
+++ TeamProject/src/main/java/com/itwillbs/controller/GuestMemberController.java
@@ -2,12 +2,17 @@ package com.itwillbs.controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.itwillbs.service.GuestMemberService;
+
 public class GuestMemberController extends HttpServlet{
+	RequestDispatcher dispatcher =null;
+	GuestMemberService guestMemberService = null;
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -23,6 +28,14 @@ public class GuestMemberController extends HttpServlet{
 	
 	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("GuestMemberController doProcess()");
+		
+		String sPath = request.getServletPath(); 
+		
+		if(sPath.equals("/login.me")) {
+			dispatcher 
+			= request.getRequestDispatcher("member/login.jsp");
+			dispatcher.forward(request, response);
+		}
 		
 	}
 }
