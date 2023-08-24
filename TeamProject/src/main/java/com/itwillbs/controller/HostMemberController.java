463c36146d93b5006ab402a3f45f12a49dafd65a
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2023-08-24 14:01:48 +0900

    login page 1 commit

--- TeamProject/src/main/java/com/itwillbs/controller/HostMemberController.java
+++ TeamProject/src/main/java/com/itwillbs/controller/HostMemberController.java
@@ -8,7 +8,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.itwillbs.service.HostMemberService;
+
 public class HostMemberController extends HttpServlet {
+	
+	RequestDispatcher dispatcher = null;
+	HostMemberService memberService = null;
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -24,6 +29,13 @@ public class HostMemberController extends HttpServlet {
 	
 	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("HostMemberController doProcess()");
-		
+		// 가상주소 뽑아오기
+		String sPath = request.getServletPath();
+		System.out.println("뽑은 가상주소 : " + sPath);
+		if(sPath.equals("/login.hme")) {
+			// member/login/login.jsp 주소변경없이 이동
+			dispatcher = request.getRequestDispatcher("member/login/login.jsp");
+			dispatcher.forward(request, response);
+		}
 	}
 }
--- TeamProject/src/main/webapp/member/login/login.jsp
+++ TeamProject/src/main/webapp/member/login/login.jsp
@@ -8,12 +8,12 @@
 </head>
 <body>
 <h1>로그인</h1>
-<form action="loginPro.me" method="post">
+<form action="loginProGuest.gme" method="post">
 <input type="radio" name="login_check" value="게스트 회원 로그인">게스트 회원 로그인<br>
-<input type="radio" name="login_check" value="호스트 회원 로그인">호스트 회원 로그인<br>
+<input type="radio" name="login_check" onclick="change_action('loginProHost.hme')" value="호스트 회원 로그인">호스트 회원 로그인<br>
 아이디 : <input type="text" name="id"><br>
 비밀번호 : <input type="password" name="pass"><br>
-<input type="submit" value="로그인"><input type="submit" value="아이디찾기"><input type="submit" value="비밀번호찾기">
+<input type="submit" value="로그인"><input type="button" value="아이디찾기"><input type="button" value="비밀번호찾기">
 </form>
 </body>
 </html>
