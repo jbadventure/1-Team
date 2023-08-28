7b9e9bc09dc8969afa53f419f37f4a9ab1d96b82
user <user@DESKTOP-LDUKGSE>
2023-08-28 17:40:05 +0900

    서평조 : review MVC, notice delete 수정

--- TeamProject/src/main/java/com/itwillbs/controller/ReviewBoardController.java
+++ TeamProject/src/main/java/com/itwillbs/controller/ReviewBoardController.java
@@ -1,13 +1,24 @@
 package com.itwillbs.controller;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+import com.itwillbs.domain.ReviewBoardDTO;
+
+import com.itwillbs.service.NoticeBoardService;
+import com.itwillbs.service.ReviewBoardService;
+
 public class ReviewBoardController extends HttpServlet{
+	
+	RequestDispatcher dispatcher = null;
+	ReviewBoardService boardService = null;
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -22,7 +33,69 @@ public class ReviewBoardController extends HttpServlet{
 	}//doPost()
 	
 	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("MemberController doProcess()");
+		String sPath = request.getServletPath();
+		System.out.println("뽑은 가상주소 : " + sPath);
+
+
+	if(sPath.equals("/reviewWrite.rbo")) {
+		dispatcher = request.getRequestDispatcher("/board/review/list.jsp");
+		dispatcher.forward(request, response);
+	} // write
+	
+	if(sPath.equals("/reviewWritePro.rbo")) {
+		request.setCharacterEncoding("utf-8");
+		boardService = new ReviewBoardService();
+		boardService.insertBoard(request);
+		response.sendRedirect("reviewList.rbo");
+	} // writePro
+	
+	if(sPath.equals("/reviewList.rbo")) {
+		boardService = new ReviewBoardService();
+		List<ReviewBoardDTO> boardList = boardService.getBoardList();
+		request.setAttribute("boardList", boardList);
+		dispatcher = request.getRequestDispatcher("/board/review/list.jsp");
+		dispatcher.forward(request, response);
+	} // list
+	
+	if(sPath.equals("/reviewContent.rbo")) {
+		System.out.println("주소 : /reviewContent.rbo");
+		boardService = new ReviewBoardService();
+		// BoardDTO boardDTO = getBoard(request) 메서드 호출
+		ReviewBoardDTO boardDTO = boardService.getBoard(request);
+		// request에 boardDTO 담기
+		request.setAttribute("boardDTO", boardDTO);
+		// content.jsp 주소변경 없이 이동
+		dispatcher = request.getRequestDispatcher("/board/review/content.jsp");
+		dispatcher.forward(request, response);
 		
-		
+	} // reviewContent
+	
+	if(sPath.equals("/reviewUpdate.rbo")) {
+		System.out.println("주소 : /reviewUpdate.rbo");
+		// BoardService 객체생성
+		boardService = new ReviewBoardService();
+		// BoardDTO boardDTO = getBoard(request) 메서드 호출
+		ReviewBoardDTO boardDTO = boardService.getBoard(request);
+		// request에 "boardDTO",boardDTO 담아서
+		request.setAttribute("boardDTO", boardDTO);
+		// center/update.jsp 주소변경없이 이동
+		dispatcher 
+		= request.getRequestDispatcher("/board/review/update.jsp");
+		dispatcher.forward(request, response);
 	}
+	
+	if(sPath.equals("/reviewUpdatePro.rbo")) {
+		System.out.println("주소 : /reviewUpdatePro.rbo");
+		// BoardService 객체생성
+		boardService = new ReviewBoardService();
+		// updateBoard(request) 메서드 호출
+		boardService.updateBoard(request);
+		// 글목록 list.bo 주소 변경 되면서 이동
+		response.sendRedirect("reviewList.rbo");
+	}
+	
+	
+	
+  } //doProcess()
 }
--- TeamProject/src/main/java/com/itwillbs/dao/ReviewBoardDAO.java
+++ TeamProject/src/main/java/com/itwillbs/dao/ReviewBoardDAO.java
@@ -1,5 +1,135 @@
 package com.itwillbs.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.itwillbs.domain.ReviewBoardDTO;
+
 public class ReviewBoardDAO {
+	
+	Connection con=null;
+	PreparedStatement pstmt=null;
+	ResultSet rs =null;
+	
+	public void dbClose() {
+		if(rs != null) {try {rs.close();} catch (SQLException e) {	}}			
+		if(pstmt != null) {try {pstmt.close();} catch (SQLException e) {	}}
+		if(con != null) {try {con.close();} catch (SQLException e) {	}}
+	}
+
+	
+
+	public void insertBoard(ReviewBoardDTO boardDTO) {
+		try {
+			con = new SQLConnection().getConnection();
+			String sql = "insert into review(reviewNum, classNum, reviewId, reviewContent, reviewFile, reviewIssueDate) values (?,?,?,?,?,?)";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, boardDTO.getReviewNum());
+			pstmt.setInt(2, boardDTO.getClassNum());
+			pstmt.setString(3, boardDTO.getReviewId());
+			pstmt.setString(4, boardDTO.getReviewContent());
+			pstmt.setString(5, boardDTO.getReviewFile());
+			pstmt.setTimestamp(6, boardDTO.getReviewIssueDate());
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		} finally {
+			dbClose();
+		}
+		
+	} // insertBoard
+
+	public int getMaxNum() {
+		int reviewNum = 0;
+		try {
+			con = new SQLConnection().getConnection();
+			String sql = "select max(reviewNum) from notice";
+			pstmt=con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				reviewNum = rs.getInt("max(noticeNum)");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			dbClose();
+		}
+		return reviewNum;
+	} // getMaxNum
+	
+	public List<ReviewBoardDTO> getBoardList() {
+		List<ReviewBoardDTO> boardList = null;
+		try {
+			con = new SQLConnection().getConnection();
+			String sql = "select * from notice order by reviewNum desc";
+			pstmt=con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			boardList = new ArrayList<>();
+			while(rs.next()) {
+				ReviewBoardDTO boardDTO = new ReviewBoardDTO();
+				boardDTO.setReviewNum(rs.getInt("reviewNum"));
+				boardDTO.setReviewId(rs.getString("reviewID"));
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return boardList;
+	}
+
+
+
+	public ReviewBoardDTO getBoard(int reviewNum) {
+		ReviewBoardDTO boardDTO = null;
+		try {
+			con = new SQLConnection().getConnection();
+			// sql 구문
+			String sql = "select * from review where num=?";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setInt(1, reviewNum);
+			// 실행
+			rs = pstmt.executeQuery();
+			// 결과 행접근 => boardDTO 객체생성 => set메서드 호출 => 열접근 데이터 저장
+			if(rs.next()) {
+				boardDTO = new ReviewBoardDTO();
+				boardDTO.setClassNum(rs.getInt("classNum"));
+				boardDTO.setReviewNum(rs.getInt("reviewNum"));
+				boardDTO.setReviewContent(rs.getString("reviewContent"));
+				boardDTO.setReviewId(rs.getString("reviewID"));
+				boardDTO.setReviewIssueDate(rs.getTimestamp("reviewIssueDate"));
+				// 첨부파일
+				boardDTO.setReviewFile(rs.getString("reviewFile"));
+			}
+			
+ 		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			dbClose();
+		}
+		return boardDTO;
+	} // getBoard
+
+
+
+	public void updateBoard(ReviewBoardDTO boardDTO) {
+		try {
+			con = new SQLConnection().getConnection();
+			// sql 구문
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
+
+
+
+	
+	
+	
 
 }
